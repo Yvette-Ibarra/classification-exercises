f86780851bd20884ba068b05a4ea77798656c6e6
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-10-17 14:54:44 -0500

    update to acquire code

--- acquire.py
+++ acquire.py
@@ -7,18 +7,30 @@ import env
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
+def new_titanic_data(): 
+    '''
+    This function reads the telco data from the Codeup db into a df.
+    '''
+    sql_query = """
+                SELECT * FROM passengers
+                """
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('titanic_db'))
+    
+    return df  
 
 def get_titanic_data():
     filename = "titanic.csv"
 
     # if file is available locally, read it
     if os.path.isfile(filename):
-        return pd.read_csv(filename)
+        return pd.read_csv(filename,index_col=0)
     
     # if file not available locally, acquire data from SQL database
     # and write it as csv locally for future use
     else:
-        df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+        df = new_titanic_data()
         
         # Write that dataframe to disk for later. Called "caching" the data for later.
         df.to_csv(filename)
@@ -26,7 +38,18 @@ def get_titanic_data():
         # Return the dataframe to the calling code
         return df  
 
+def new_iris_data(): 
+    '''
+    This function reads the iris data from the Codeup db into a df.
+    '''
+    sql_query = """
+                SELECT * FROM measurements JOIN species USING(species_id)
+                """
     
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('iris_db'))
+    
+    return df   
 
 
 def get_iris_data():
@@ -34,15 +57,15 @@ def get_iris_data():
 
     # if file is available locally, read it
     if os.path.isfile(filename):
-        return pd.read_csv(filename)
+        return pd.read_csv(filename ,index_col=0)
     
     # if file not available locally, acquire data from SQL database
     # and write it as csv locally for future use
     else:
-        df = pd.read_sql('SELECT * FROM measurements JOIN species USING(species_id);', get_connection('iris_db'))
+        df = get_iris_data()
         
         # Write that dataframe to disk for later. Called "caching" the data for later.
-        df.to_csv(filename)
+        df.to_csv(filename, index = False)
 
         # Return the dataframe to the calling code
         return df 
